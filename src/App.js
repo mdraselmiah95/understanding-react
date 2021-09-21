80aec4c218d898ea1881e252dbe2dad35f731a35
rasel <russelroy15@gmail.com>
2021-09-21 15:00:56 +0600

    WIP on understanding react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Device from "./components/Device/Device";
 
 function App() {
   return (
     <div className="App">
+      <Device name="phone" price="13500" />
       <h3>npm= Node Package Manager </h3>
       <h4>command-line interface</h4>
       <button>CLICK ME</button>
