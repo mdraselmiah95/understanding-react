ebab74d5e99f5c1003838625ed6d7e912e95ea2a
rasel <russelroy15@gmail.com>
2021-09-21 23:03:42 +0600

    WIP on understanding React

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import "./App.css";
 import Device from "./components/Device/Device";
 
 function App() {
-  const [steps, setSetsps] = useState([]);
+  const [steps, setSteps] = useState(0);
+  const handleIncreaseSteps = () => {
+    const newSteps = steps + 1;
+    setSteps(newSteps);
+  };
   return (
     <div className="App">
       <Device name="phone" price="13500" />
       <h3>npm= Node Package Manager </h3>
       <h4>command-line interface</h4>
+
+      <h3>My Steps:{steps}</h3>
+      <button onClick={handleIncreaseSteps}>Walk</button>
+      <br />
       <button>CLICK ME</button>
     </div>
   );
