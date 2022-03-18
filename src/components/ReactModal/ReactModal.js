9321519b448acc8dd7d5e600400cc9413d84fcf6
mdraselmiah95 <russelroy15@gmail.com>
2022-03-18 20:17:42 +0600

    new model added and practice

--- src/components/ReactModal/ReactModal.js
+++ src/components/ReactModal/ReactModal.js
@@ -15,9 +15,7 @@ const ReactModal = () => {
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                   <h3 className="text-3xl font-semibold">Modal Title</h3>
                   <button
@@ -29,6 +27,7 @@ const ReactModal = () => {
                     </span>
                   </button>
                 </div>
+
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
